bbb4bad6b6727be02a151770af1c504fee63f000
kamesh952 <rkameshraj7@gmail.com>
2025-06-27 08:02:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/home";
+import About from "./pages/about/about";
+import History from "./pages/history/history";
+import Contact from "./pages/contact/contact";
+import FAQ from "./pages/faq/faq";
+import Cancel from "./pages/cancel/cancel";
+import Terms from "./pages/terms/terms";
+import Privacy from "./pages/priv/priv";
+import Login from "./pages/Home/login";
+import Flight from "./pages/flight/flight"
+import Rent from "./pages/rent/rental";
+import AdminDashboard from "./pages/admin/admin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/cancel" element={<Cancel />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/flight" element={<Flight />} />
+          <Route path="/rentals" element={<Rent />} />
+          <Route path="/admin" element={< AdminDashboard/>} />
+          
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
